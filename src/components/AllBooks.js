deb09d4318ae74fa3a908f044c6a187e161cb128
Raanan <raanango@post.bgu.ac.il>
2021-03-15 18:15:46 +0200

    Allbooks file changed to a functional component, page is updated when an item is removed

--- src/components/AllBooks.js
+++ src/components/AllBooks.js
@@ -1,47 +1,74 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Book from './Book';
-class AllBooks extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      books: [],
-      times: []
-    }
+function AllBooks()  {
+  
 
-  }
-  componentWillMount(){
-    axios.get('http://localhost:8080/media/all')
+    const [books,setBooks] = useState(() => getBooks()) 
+    const [numberOfBooks, setBooksNumber] = useState(1)
     
-    .then(response  => {
-      this.setState({
+    function getBooks(){
+      console.log("getting")
+      axios.get(`http://localhost:8080/media/all/`)
     
-        books: response.data,
-        times: response.data[0].times_borrowed
-        
-      });
+      .then(response  => {
       
+        setBooks(prevBooks => response.data)
+      
+      });
     
-    })
   }
-  // componentDidMount(){
-  //   axios.get('http://localhost:8080/media/all')
-    
-  // }
-  render(){
+  useEffect(() => {
+    console.log("render")
+
+  }, [books])
     
       return (
-        <div>
+        <>
           
-          {this.state.books.map(book =>
-            <Book key = {book.id}
-              book = {{name: book.name, id: book.id, timesBorrowed: book.timesBorrowed}} >
-
-            </Book>  
+          <button onClick = {getBooks} > get books </button> 
+          {books==null? "loading" : 
+          books.map(book =>
+            <>
+              <Book key = {book}
+              book = {{name: book.name, id: book.id, timesBorrowed: book.timesBorrowed}} 
+              >
+              
+              </Book>  
+              <button id={book.id + 30}  onClick = {() => removeBook(book.id)}>remove</button>
+              
+            </>
+            
           )}
-        </div>
+          
+          
+        </>
     )
+  
+
+  function removeBook(id){
+    console.log("start remove")
+    axios.delete(`http://localhost:8080/media/delete/${id}`)
+    
+      
+      .then((response ) => {
+        console.log("after remove, this should start getting the books again")
+        getBooks()
+        console.log("new books probably re- fetched")
+      })
+      
+      // .catch((error) => {
+      //   console.log("something went wrong")
+      // });
+    
+    
   }
 }
+
 export default AllBooks
+
+            {/* <BookExpandedDetails></BookExpandedDetails> */}
+              
+       
+   
 //com
